0a4e2cc687d2ca185dcfd8782db443ccbf0c34ba
Olivia Chang <olivia.chang@limelighthealth.com>
2020-07-25 13:57:40 -0700

    first commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
+
 const pluginRss = require('@11ty/eleventy-plugin-rss')
 const pluginNavigation = require('@11ty/eleventy-navigation')
 const markdownIt = require('markdown-it')
+const axios = require('axios')
 
 const filters = require('./utils/filters.js')
 const transforms = require('./utils/transforms.js')
@@ -25,6 +28,42 @@ module.exports = function (config) {
     // Shortcodes
     Object.keys(shortcodes).forEach((shortcodeName) => {
         config.addShortcode(shortcodeName, shortcodes[shortcodeName])
+    })
+
+    const allTags = []
+
+    config.addCollection('quotes', async (collection) => {
+        let response = await axios.get(process.env.API_URL)
+        collection = response.data
+
+        collection.map((quote) => {
+            quote.text = quote['fields']['Quote']
+
+            quote.tags = quote['fields']['Tags']
+            if (quote.tags) {
+                quote.tags.forEach((tag) => {
+                    if (!allTags.includes(tag)) allTags.push(tag)
+                })
+            }
+
+            quote.source = quote['fields']['Source (Newspaper)']
+            quote.author = quote['fields']['Author']
+            quote.link = quote['fields']['Link']
+            quote.date = new Date(quote['created'])
+            return quote
+        })
+
+        collection.sort((a, b) => (a.date < b.date ? 1 : -1))
+
+        return collection
+    })
+
+    config.addCollection('tags', function (collection) {
+        collection = allTags
+
+        collection.sort((a, b) => (a < b ? 1 : -1))
+
+        return collection
     })
 
     // Icon Sprite
